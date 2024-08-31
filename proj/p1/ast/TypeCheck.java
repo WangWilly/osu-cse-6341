811d88bb5c0948c5b108c1cfb5a7eb5cbb415c8f
WangWilly <s89667654@gmail.com>
2024-08-31 12:41:29 -0400

    feat: 🎸 typecheck for decl

--- proj/p1/ast/TypeCheck.java
+++ proj/p1/ast/TypeCheck.java
@@ -2,30 +2,36 @@ package ast;
 import java.util.HashMap;
 import java.util.Map;
 
-import ast.VarDecl;
+public final class TypeCheck {
+    private Map < String, ValueMeta > symbolTable;
 
-// public final class TypeCheck {
-//     private Map < String, ValueMeta > symbolTable;
+    public TypeCheck() {
+        this.symbolTable = new HashMap < String, ValueMeta > ();
+    }
 
-//     public TypeCheck() {
-//         this.symbolTable = new HashMap < String, ValueMeta > ();
-//     }
+    public void addSymbol(String name, ValueMeta meta) {
+        this.symbolTable.put(name, meta);
+    }
 
-//     public void addSymbol(String name, ValueMeta meta) {
-//         this.symbolTable.put(name, meta);
-//     }
+    public ValueMeta getSymbol(String name) {
+        return this.symbolTable.get(name);
+    }
 
-//     public ValueMeta getSymbol(String name) {
-//         return this.symbolTable.get(name);
-//     }
+    ////////////////////////////////////////////////////////////////////////////
 
-//     ////////////////////////////////////////////////////////////////////////////
+    public boolean checkIntVarDecl(IntVarDecl varDecl) {
+        if (this.symbolTable.containsKey(varDecl.ident)) {
+            return false;
+        }
+        this.symbolTable.put(varDecl.ident, new ValueMeta(varDecl.ident, ValueMeta.ValueType.INT));
+        return true;
+    }
 
-//     public boolean checkVarDecl(VarDecl varDecl) {
-//         if (this.symbolTable.containsKey(varDecl.ident)) {
-//             return false;
-//         }
-//         this.symbolTable.put(varDecl.ident, new ValueMeta(varDecl));
-//         return true;
-//     }
-// }
+    public boolean checkFloatVarDecl(FloatVarDecl varDecl) {
+        if (this.symbolTable.containsKey(varDecl.ident)) {
+            return false;
+        }
+        this.symbolTable.put(varDecl.ident, new ValueMeta(varDecl.ident, ValueMeta.ValueType.FLOAT));
+        return true;
+    }
+}
