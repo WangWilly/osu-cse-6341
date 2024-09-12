f84cb3bdc1f69b70504d8f272c11f1c8cffb87ec
WangWilly <s89667654@gmail.com>
2024-09-12 12:12:23 -0400

    feat: p2 first draft

--- proj/p2/interpreter/Interpreter.java
+++ proj/p2/interpreter/Interpreter.java
@@ -4,30 +4,48 @@ import java.io.*;
 import java.util.*;
 import parser.ParserWrapper;
 import ast.Program;
+import ast.TypeCheck;
+
+////////////////////////////////////////////////////////////////////////////////
 
 public class Interpreter {
-
     // Process return codes
     public static final int EXIT_SUCCESS = 0;
     public static final int EXIT_PARSING_ERROR = 1;
     public static final int EXIT_STATIC_CHECKING_ERROR = 2;
+
+    ////////////////////////////////////////////////////////////////////////////
+
     public static void main(String[] args) {
-        String filename = args[0];
-        Program astRoot = null;
-        BufferedReader reader;
-        try {
-            reader = new BufferedReader(new FileReader(filename));
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
+            String filename = args[0];
+            Program astRoot = null;
+            BufferedReader reader;
+            try {
+                reader = new BufferedReader(new FileReader(filename));
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+            try {
+            astRoot = ParserWrapper.parse(reader, filename);
+            } catch (Exception ex) {
+                Interpreter.fatalError("Uncaught parsing error: " + ex, EXIT_PARSING_ERROR);
+            }
+        // for debugging
+        astRoot.print(System.out);
+
+        // type checking. If the program does not typecheck,
+        // call fatalError with return code EXIT_STATIC_CHECKING_ERROR
+        TypeCheck typeCheck = new TypeCheck();
+        if (!astRoot.checkType(typeCheck)) {
+            Interpreter.fatalError(
+                "Uncaught static checking error",
+                Interpreter.EXIT_STATIC_CHECKING_ERROR
+            );
         }
-        try {
-	    astRoot = ParserWrapper.parse(reader, filename);
-        } catch (Exception ex) {
-            Interpreter.fatalError("Uncaught parsing error: " + ex, EXIT_PARSING_ERROR);
-        }
-	// for debugging
-	astRoot.print(System.out);
     }
+
+    ////////////////////////////////////////////////////////////////////////////
+
     public static void fatalError(String message, int processReturnCode) {
         System.out.println(message);
         System.exit(processReturnCode);
